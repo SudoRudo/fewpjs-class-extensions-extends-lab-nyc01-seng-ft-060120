13ae252c6881bf32757bc7051b039a99a64da1da
Rudy Guerrero <r.guerreronyc@gmail.com>
2020-07-27 02:50:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Your code here
+class Polygon {
+  constructor(array){
+    this.sidesArray = array
+  }
+  
+  get countSides(){
+    return this.sidesArray.length
+  }
+  
+  get perimeter(){
+    return this.sidesArray.reduce(function(a, b){
+        return a + b;
+    }, 0);
+  }
+}
+
+class Triangle extends Polygon{
+  
+}
